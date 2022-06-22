be0813f8c78217dab5d84411d7e720edee021671
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-06-22 12:50:38 +0100

    changed the db config

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://kvjksnthxbusgr:ef61fa014b30f99c3741751a6080e46746cd01f219f8246b4d05bd49f4c16872@ec2-54-147-33-38.compute-1.amazonaws.com:5432/d56n6ommgaf7ic'
+SQLALCHEMY_DATABASE_URI = 'postgres://jcmwuirpcjmcgs:0e0577e17196f9a2186c3dfdaeb7e1150e7b718ed227a5089ddae976875adbb3@ec2-52-72-56-59.compute-1.amazonaws.com:5432/dckqc79qhsaamu'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
